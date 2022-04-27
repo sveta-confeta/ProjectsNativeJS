69da57dfe313055dbc17401bb6384b1bc064744c
sveta-confeta <_svet_@tut.by>
2022-04-27 19:13:06 +0300

    3 lesson/Promises

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -1,47 +1,65 @@
+import {log} from "util";
+
 console.log('lesson 2');
 
-// Lexical environment
-// http://jsflow.org/docs/lex-env/
-
-//// Closure
-// https://learn.javascript.ru/closure
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Closures
-// https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%BD%D0%B8%D0%BC%D0%B0%D0%B5%D0%BC-%D0%B7%D0%B0%D0%BC%D1%8B%D0%BA%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B2-javascript-%D1%80%D0%B0%D0%B7-%D0%B8-%D0%BD%D0%B0%D0%B2%D1%81%D0%B5%D0%B3%D0%B4%D0%B0-c211805b6898
-// https://www.youtube.com/watch?v=pahO5XjnfLA
-
-//// Сurrying
-// https://learn.javascript.ru/currying-partials
-// https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%BD%D0%B8%D0%BC%D0%B0%D0%B5%D0%BC-%D0%BA%D0%B0%D1%80%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D0%B2-javascript-5ec4a1d88827
-
-// Pattern Module
-// https://habr.com/ru/company/ruvds/blog/419997/
-
-// Recursion
-// https://learn.javascript.ru/recursion
-// https://www.youtube.com/watch?v=Kuq6oIN3PH0
 
 
-// Task 01
+//Карирование:
 // Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
+function sum(num:number){
+    return function sum2(num2:number){
+        return num+num2;
+    }
+}
+//Рекурсия
+function sumRecursion(num:number):number{ //например нам надо выходить из рекурсии когда мы дошли до 1
+    if(num ===1) return num //достигли 1 ,вернули 1 и вышли// если num =1,то просто верните 1
+    return num+sumRecursion( num-1);
 
-// Task 02
-// Реализовать функцию makeCounter которая работает следующим образом:
-// const counter = makeCounter();
-// counter(); // 1
-// counter(); // 2
-// const counter2 = makeCounter();
-// counter2(); // 1
-// counter(); // 3
+}
+console.log(sumRecursion(100));
+
+//Замыкание
+function makeCounter() {
+    let count = 0;
+
+    return function() {
+        return ++count; // есть доступ к внешней переменной "count"
+    };
+}
+
+let counter = makeCounter();
+console.log(counter()); // 1
+console.log(counter()); // 2
+const counter2 = makeCounter();
+console.log(counter2()); // 1
+console.log(counter()); // 3
+
+
 
 // Task 03
 // Переписать функцию из Task 02 так, что бы она принимала число в качестве аргумента и это число было стартовым значением счетчика
 // и возвращала следующий объект методов:
+
+function numberCounter(num:number) {
+    let count = num;
+
+
+    return  function() {
+        return count--;
+    };
+    return function() {
+        return count++ // есть доступ к внешней переменной "count"
+    };
+
+}
+
 // increase: +1
 // decrease: -1
 // reset: установить счетчик в 0;
 // set: установить счетчик в заданное значение;
 
-// Task 04*
+
 // Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
 // и что бы корректно работали следующие вызовы:
 // 1) superSum(0) //0
@@ -51,13 +69,57 @@ console.log('lesson 2');
 // 5) superSum(3)(2,5)(3) //10
 // 6) superSum(3)(2,5)(3,9) //10
 
-// P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
+//1)
+function superSum (n:number){
+   if (n<=0){
+       return 0;
+}
+}
+///2)
+function superSum2 (n:number){
+   let otherParams:Array<number>=[];
+   function inner (...arg:number[]){
+       otherParams=[...otherParams,...arg]
+       if(otherParams.length>=n){
+           otherParams.length=n;
+           return otherParams.reduce((acc,el)=>acc+el)
+       }else {
+           return inner;
+       }
+   }
+   return inner;
+}
+
+
+
 
 // Task 05
-// решить все задачи по рекурсии которые даны в конце статьи https://learn.javascript.ru/recursion
+//рекурсия
+
+function sumTo(n:number){
+    let result=0;
+    for(let i=1;i>=n;n++){
+        result=result +i;
+    }
+    return result;
+}
+let i=1;
+function sumToRecursion(n:number){
+console.log(i);
+i++;
+if(i<n){
+    sumToRecursion(n)
+}
+
+}
+sumToRecursion(5)
 
 // Task 06
 // написать функцию, которая повторяет функционал метода flat массива на всю глубину.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
 
 // just a plug
 export default () => {};
+
+
+//https://learn.javascript.ru/task/filter-through-function
--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,4 +1,5 @@
-console.log('lesson 3');
+import {rejects} from "assert";
+
 
 // Event loop
 // https://learn.javascript.ru/event-loop
@@ -14,7 +15,73 @@ console.log('lesson 3');
 
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
+//имитация fetch
+let prom = new Promise((res, rej) => { //создание нового промиса с двумя колбэками res,rej//функция конструктор
+    setTimeout((response) => {
+        if (response.httpStatus >= 200 && response.httpStatus < 400) { //проверка что запрос пришел без ошибок
+            res(response.data); //вызываем функцию res и отдаем ей результат запроса.и теперь в prom доступны результаты запроса
+        } else {
+rej(response.error);
+        }
+    }, 2000, {httpStatus: 200, data: {userName: 'Sveta', id: 1, status: 'active'}, error: {}})//сделаем имитацию запроса.третьим параметом в сеттаймаут попадают аргументы,которые будут приходить в функцию в момент вызова
+});
+//console.log(prom) //новый промис в состоянии panding со значением undefined
+// когда промис зарезолвится, мы не можем напрямую получать данные-только с помощью методов:
+//смоделируем случай: в результате запроса пллучили id и по этому id делаем второй запрос на другой сервер:
+prom.then(
+    res => {
+    return new Promise((res, rej) => { //создание нового промиса с двумя колбэками res,rej//функция конструктор
+        setTimeout((response) => {
+            if (response.httpStatus >= 200 && response.httpStatus < 400) { //проверка что запрос пришел без ошибок
+                res(response.data); //вызываем функцию res и отдаем ей результат запроса.и теперь в prom доступны результаты запроса
+            } else {
+                rej(response.error);
+            }
+        }, 1200, {httpStatus: 200, data: { id: 1, count:68786898}, error: {}})//сделаем имитацию запроса.третьим параметом в сеттаймаут попадают аргументы,которые будут приходить в функцию в момент вызова
+    });
+    },err =>{ //это вторпой колбэк в методе зен и он срабатывает когда у нас промис reject
+
+        console.log(err)
+    })
+    .then(res2 => {
+       console.log(res2);
+    },err2=>{      //второй колбэк отвечает за  rejecta. если не укажем, то на реджект зен  не сработает
+        console.log(err2);
+    })
+    // .then(null,err3=>{
+    //     console.log(err3)  //заглушка для перехвата ошибок
+    // })
+    .catch(err=>{     //более современный метод вместо заглушки
+        console.log(err)
+    })
 
 
-// just a plug
-export default ()=>{};
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json()) //это метод который возращает данные для нас
+    .then((json)=>{
+        console.log(json);
+        return fetch('https://jsonplaceholder.typicode.com/posts')
+    } )
+    .then((resp)=>resp.json())
+    .then((json) =>{
+        console.log(json)
+        return fetch('https://jsonplaceholder.typicode.com/posts')
+    })
+
+///////////////////////////////////////////////////////////////
+
+let promise = new Promise((res, rej) => { //создание нового промиса с двумя колбэками res,rej//функция конструктор
+    setTimeout((response) => {
+        if (response.httpStatus >= 200 && response.httpStatus < 400) { //проверка что запрос пришел без ошибок
+            res(response.data); //вызываем функцию res и отдаем ей результат запроса.и теперь в prom доступны результаты запроса
+        } else {
+            rej(response.error);
+        }
+    }, 2000, {httpStatus: 200, data: {userName: 'Sveta', id: 1, status: 'active'}, error: {}})//сделаем имитацию запроса.третьим параметом в сеттаймаут попадают аргументы,которые будут приходить в функцию в момент вызова
+});
+
+promise
+    .then(console.log) //пришел ответ и его сразу в console.log запихнули. console.log
+// ничего не возращает  -поэтому следующий зен будет undefined
+
+
